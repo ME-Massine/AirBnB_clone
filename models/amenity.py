1d914008739f27bcaf0a385aeeedd8614d114572
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2023-08-08 17:11:42 +0100

    Created amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for the CustomFeature class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """A class representing Amenity of a location or property
+
+    Attributes:
+        aname(str): amenity name
+
+    """
+    aname = ""
