af74cadff65d9db9fbf0f5cd272c8e2e4bc5ba20
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2023-08-12 04:50:02 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module defining the State characteristics."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Describes a geographical state or province.
+
+    Attributes:
+        name (str): Designation of the state.
+    """
+  name = ""
